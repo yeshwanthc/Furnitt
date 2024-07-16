fa420954cd023620a444b85dd202c8bccf81fa0a
yeshwanth1837 <yeshwanthnaidu62@gmail.com>
2024-07-16 20:18:06 +0530

    issues

--- src/hooks/UseScrollDirection.tsx
+++ src/hooks/UseScrollDirection.tsx
@@ -1,7 +1,7 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 
-const useScrollDirection = () => {
-  const [scrollDirection, setScrollDirection] = useState(null);
+const useScrollDirection = (): "up" | "down" | null => {
+  const [scrollDirection, setScrollDirection] = useState<"up" | "down" | null>(null);
 
   useEffect(() => {
     let lastScrollY = window.pageYOffset;
@@ -9,7 +9,10 @@ const useScrollDirection = () => {
     const updateScrollDirection = () => {
       const scrollY = window.pageYOffset;
       const direction = scrollY > lastScrollY ? "down" : "up";
-      if (direction !== scrollDirection && (scrollY - lastScrollY > 10 || scrollY - lastScrollY < -10)) {
+      if (
+        direction !== scrollDirection &&
+        (scrollY - lastScrollY > 10 || scrollY - lastScrollY < -10)
+      ) {
         setScrollDirection(direction);
       }
       lastScrollY = scrollY > 0 ? scrollY : 0;
