20b2f8ebc616dee28e2fa1a6434b1334b657d892
yeshwanth1837 <yeshwanthnaidu62@gmail.com>
2024-07-27 19:20:23 +0530

    changes

--- src/app/about/HomeAbout.tsx
+++ src/app/about/HomeAbout.tsx
@@ -7,7 +7,7 @@ const AboutUs = () => {
   let heading = "About Us";
   let description =
     "Furnitt is a professional interior design firm dedicated to infusing creativity and artistic flair into your home. We believe your home is your castle and should reflect your personality, making you swell with pride. As enablers of beautiful living spaces, we have been adding peace, style, and class to countless homes since our foundation, and we can do the same for you. For us, your home is not just a space needing an authentic touch; it's a place where you share moments with loved ones. We blend creativity, design, luxury, and architecture to give your home a splendid, heartwarming, and glamorous look. When it comes to designing a beautiful home, you deserve the best in innovation. From the moment you engage our services, we deliver perfection. You will experience flexibility, freedom, and extreme professionalism from the initial liaison to the final product. Contact us today to see how we can transform your home.";
-  let imageSrc = "/images/poster.jpg";
+  let imageSrc = "/images/image.jpeg";
   let imageAlt = "About Us";
   return (
     <div className=" py-[60px]">
